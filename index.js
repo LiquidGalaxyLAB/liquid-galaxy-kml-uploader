d3929ec9014e2af2983e7cdcbd40652192a1fa1a
Eric Monne Mesalles <emonne97@gmail.com>
2019-07-04 10:26:47 +0200

    split the documentation

--- index.js
+++ index.js
@@ -3,10 +3,85 @@ const lgKML = express.Router()
 var path = require('path');
 const kmlDir = path.join(require('os').homedir(),'/kmlApi');
 var fs = require('fs');
-
+var kmlWriter = require('kmlWriter')
+const kml = kmlWriter()
 
 var kmlList = []
 var currentKml = 0;
+
+
+/***
+* KML Builder endpoits
+****/
+lgKML.post('/kml/builder/addplacemark',function(req,res){
+
+})
+lgKML.post('/kml/builder/Createtour',function(req,res){
+  console.log(req.params())
+})
+lgKML.post('/kml/builder/addpoint/:tourName',function(req,res){
+
+})
+
+
+/***
+* KML Manage endpoints
+****/
+lgKML.get('/kml/manage/current',function(req,res){
+  res.send(currentKml)
+})
+lgKML.get('/kml/manage/list',function(req,res){
+  res.send(kmlList)
+})
+lgKML.put('/kml/manage/:id',function(req,res){
+  console.log(req.params)
+  currentKml = req.params.id
+  res.send("okay!")
+})
+lgKML.put('/kml/manage',function(req,res){
+  checkFolder().then(() => {
+    console.log(kmlList[0])
+    res.send(kmlList)
+  })
+
+})
+lgKML.delete('/kml/manage/:id',function(req,res){
+  if(kmlList.length > 0){
+    console.log(kmlList[req.params.id].path)
+    fs.unlink(kmlList[req.params.id].path,function(err){
+      console.log(err)
+    })
+    checkFolder().then(() => {
+      res.send(kmlList)
+    })
+  }else{
+    res.send(kmlList)
+
+  }
+
+})
+
+/****
+*
+****/
+lgKML.get('/kml/upload/',function(req,res){
+
+})
+
+
+
+/****
+* the endpoint to sync the kml
+****/
+lgKML.get('/kml/viewsync',function(req,res){
+  res.setHeader('Content-Type', 'text/xml')
+  console.log(kmlList[currentKml].path)
+  res.sendFile(kmlList[currentKml].path)
+  console.log("asking for kml")
+})
+
+
+//suport functions
 
 function checkFolder(){
   kmlList = []
@@ -28,85 +103,7 @@ function addKML(kml){
     'path'  : path.join(kmlDir,kml)
     })
 }
-/***
-* KML Builder endpoits
-****/
-lgKML.get('/kml/Builder/addplacemark',function(req,res){
-  // '<Placemark>'
-  //   "<name>" "</name>"
-  //   '<description>Some Descriptive text.</description>'
-  //   '<LookAt>'
-  //     '<longitude>'long'</longitude>'
-  //     '<latitude>'lat'</latitude>'
-  //     '<range>' range '</range>'
-  //     '<tilt>' tilt '</tilt>'
-  //     '<heading>'2.7'</heading>'
-  //   '</LookAt>'
-  //   '<Point>'
-  //     '<coordinates>' + -90.86948943473118,48.25450093195546,0 +'</coordinates>'
-  //   '</Point>'
-  // '</Placemark>'
-})
-lgKML.post('/kml/Builder/Createtour',function(req,res){
-  console.log(req.params())
-})
-lgKML.get('/kml/Builder/addpoint/:tourName',function(req,res){
 
-})
-/***
-* KML Manage endpoints
-****/
-lgKML.get('/kml/manage/current',function(req,res){
-  res.send(currentKml)
-})
-lgKML.get('/kml/manage/change/:id',function(req,res){
-  console.log(req.params)
-  currentKml = req.params.id
-  res.send("okay!")
-})
-lgKML.get('/kml/manage/list',function(req,res){
-  res.send(kmlList)
-})
-lgKML.get('/kml/manage/delete/:id',function(req,res){
-  if(kmlList.length > 0){
-    console.log(kmlList[req.params.id].path)
-    fs.unlink(kmlList[req.params.id].path,function(err){
-      console.log(err)
-    })
-    checkFolder().then(() => {
-      res.send(kmlList)
-    })
-  }else{
-    res.send(kmlList)
-
-  }
-
-})
-lgKML.get('/kml/manage/update',function(req,res){
-  checkFolder().then(() => {
-    console.log(kmlList[0])
-    res.send(kmlList)	
-  })
-	
-})
-/****
-*
-****/
-lgKML.get('/kml/upload/',function(req,res){
-
-})
-
-
-
-/****
-* the endpoint to sync the kml
-****/
-lgKML.get('/kml/viewsync',function(req,res){
-  res.setHeader('Content-Type', 'text/xml')
-  console.log(kmlList[currentKml].path)
-  res.sendFile(kmlList[currentKml].path)
-  console.log("asking for kml")
-})
 
 
 checkFolder()
